edf5fed1c4e18acbda4ab9787a9deb29c55639a3
Mwiti Isaac <mwitiisaac14@gmail.com>
2026-08-11 08:17:55 +0300

    add new theme colors, enhance background textures, and improve accessibility for reduced motion preferences

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,32 +1,26 @@
 import { Navbar } from "@/components/Navbar";
 import { HeroSection } from "@/components/HeroSection";
 import { AboutSection } from "@/components/AboutSection";
-import { FacilityMaskSection } from "@/components/FacilityMaskSection";
 import { ImpactStrip } from "@/components/ImpactStrip";
+import { TrainingShowcase } from "@/components/TrainingShowcase";
 import { CurriculumSection } from "@/components/CurriculumSection";
-import { VibeSection } from "@/components/VibeSection";
 import { ChancenSection } from "@/components/ChancenSection";
 import { CareerSection } from "@/components/CareerSection";
 import { MomentsSection } from "@/components/MomentsSection";
 import { TestimonialsSection } from "@/components/TestimonialsSection";
 import { AdmissionsSection } from "@/components/AdmissionsSection";
 import { Footer } from "@/components/Footer";
-import { CurveDivider, AngleDivider } from "@/components/SectionDividers";
 
 export default function Home() {
   return (
     <main className="min-h-screen flex flex-col bg-white overflow-x-hidden">
       <Navbar />
       <HeroSection />
-      <CurveDivider fillColor="#FCF8E6" className="-mt-12 z-20" />
       <AboutSection />
-      <FacilityMaskSection />
       <ImpactStrip />
+      <TrainingShowcase />
       <CurriculumSection />
-      <CurveDivider fillColor="#0F172A" className="-mt-8 z-20" />
-      <VibeSection />
       <ChancenSection />
-      <AngleDivider fillColor="#FCF8E6" className="z-20" />
       <CareerSection />
       <MomentsSection />
       <TestimonialsSection />
